cd599eb4d941274305832a8817301a9494e91981
Benjamin Cheng <ben@bcheng.me>
2020-10-27 11:34:24 -0600

    finish subjective_bot

--- a5/subjective_bot.py
+++ a5/subjective_bot.py
@@ -15,3 +15,53 @@
 
         Enter a sentence
 """
+
+import collections
+import readline
+import spacy
+import torch
+import torchtext
+
+import models
+
+def load_vocab():
+    field_text = torchtext.data.Field(sequential=True, lower=True, tokenize='spacy', include_lengths=True)
+    field_label = torchtext.data.Field(sequential=False, use_vocab=False)
+    fields = [('text', field_text), ('label', field_label)]
+    data = torchtext.data.TabularDataset.splits(
+            path='./data', train='train.tsv', validation='validation.tsv', test='test.tsv',
+            format='TSV', skip_header=True, fields=fields,
+    )
+
+    field_text.build_vocab(*data)
+    field_text.vocab.load_vectors(torchtext.vocab.GloVe(name='6B', dim=100))
+    return field_text.vocab
+
+def tokenizer(text):
+    spacy_en = spacy.load('en')
+    return [tok.text for tok in spacy_en(text)]
+
+def classify(model, token_ints):
+    token_tensor = torch.LongTensor(token_ints).view(-1, 1)
+    lengths = torch.Tensor([len(token_ints)])
+    pred = torch.sigmoid(model(token_tensor, lengths))
+    return 'subjective' if pred > 0.5 else 'objective', float(pred)
+
+baseline = torch.load('./model_baseline.pt')
+cnn = torch.load('./model_cnn.pt')
+rnn = torch.load('./model_rnn.pt')
+
+vocab = load_vocab()
+
+while True:
+    sentence = input('Enter a sentence\n')
+    tokens = tokenizer(sentence.lower())
+    token_ints = [vocab.stoi[tok] for tok in tokens]
+
+    cat, prob = classify(baseline, token_ints)
+    print(f'Model baseline: {cat} ({prob:.3f})')
+    cat, prob = classify(cnn, token_ints)
+    print(f'Model cnn: {cat} ({prob:.3f})')
+    cat, prob = classify(rnn, token_ints)
+    print(f'Model rnn: {cat} ({prob:.3f})')
+
